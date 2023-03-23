99e8383d304926eb61f98dd73642354b65d07d62
tuanhm03 <tuanthanhcong2027@gmail.com>
2023-03-23 12:38:26 +0700

    start_angular_route

--- src/js/script.js
+++ src/js/script.js
@@ -4,24 +4,3 @@ window.addEventListener('scroll' , () => {
     menuHelp.classList.toggle('hiddenMenuHelp', window.scrollY > 0);
     menuMain.classList.toggle('mt-3', window.scrollY == 0);
 });
-// window.addEventListener('load', (event) => {
-//     let myAleart = document.querySelector('.toast');
-//     let bsAleart = new bootstrap.Toast(myAleart);
-//     bsAleart.show();
-// });
-// var app = angular.module("myApp", ["ngRoute"]);
-// app.config(function($routeProvider) {
-//     $routeProvider
-//     .when("/", {
-//       templateUrl : "main.html"
-//     })
-//     .when("/red", {
-//       templateUrl : "red.html"
-//     })
-//     .when("/green", {
-//       templateUrl : "green.html"
-//     })
-//     .when("/blue", {
-//       templateUrl : "blue.html"
-//     });
-//   });
